10836bef9e5bc845295123c33a67a7485461366f
Bharat Kumar <bharatkumar@Bharats-MacBook-Air.local>
2025-07-07 16:18:15 +0800

    updated code

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-
+import React from "react"
 
 const AppLayout = ()=>{
-    
+
+    return (
+        <div>
+            <h1>Namaste React</h1>
+            <p>Welcome to the Namaste React course!</p>
+        </div>
+    )
 }
