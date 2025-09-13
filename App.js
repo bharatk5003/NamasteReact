61e5e7e28e5833f374acdd5dfc2ca40b0221f5b6
Bharat Kumar <bharatkumar@fynd-external.com>
2025-09-13 17:06:55 +0530

    server started

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const AppLayout = ()=>{
+const Header = () => {
+  return (
+    <div className="heading">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://cdn.pixelbinz0.de/v2/yellow-fog-88d971/original/delvery.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul className="list-items">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>Namaste React</h1>
-            <p>Welcome to the Namaste React course!</p>
-        </div>
-    )
-}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
---
+++ README.md
@@ -0,0 +1,19 @@
+# Namaste React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Catching - Faster Builds
+- Image Optimisation
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+-  Differential Bundling  - support older browser
+- Diagnostic
+- HTTPs
+- Tree Shaking  - remove unused code
+- Different dev and prod bundles
