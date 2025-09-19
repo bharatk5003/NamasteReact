0ac02790ef3dc2f8514f66a4aafca25430fd54ee
Bharat Kumar <bharatkumar@fynd-external.com>
2025-09-19 12:24:27 +0530

    shimmer UI added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { RESTAURANT_API } from "../utils/constants";
+import Shimmer from "./Shimmer";
 const Body = () => {
   const [restaurantList, setRestaurantList] = useState([]);
   const [nextOffset, setNextOffset] = useState(null);
   const [isLoadingMore, setIsLoadingMore] = useState(false);
 
   useEffect(() => {
+    console.log("useEffect called");
     fetchRestaurant();
   }, []);
 
@@ -22,10 +24,38 @@ const Body = () => {
     setNextOffset(offset);
   };
 
+  const filter = () => {
+    const res = restaurantList.filter((restaurant) => {
+      return restaurant.info.avgRating >= 4.7;
+    });
+    setRestaurantList(res);
+  };
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    // console.log(value);
+    const resData = restaurantList.filter((restaurant) => {
+      return restaurant.info.name.toLowerCase().includes(value.toLowerCase());
+    });
+    console.log(resData);
+    setRestaurantList(resData);
+  };
+  if (restaurantList.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
+      <div className="filter-restaurant">
+        <button type="button" onClick={filter}>
+          Filter
+        </button>
+      </div>
       <div className="search">
-        <input type="text" placeholder="Search..."></input>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={handleInputChange}
+        ></input>
       </div>
       <div className="res-container">
         {restaurantList.map((restaurant) => {
